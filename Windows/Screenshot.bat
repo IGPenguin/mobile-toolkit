6b9e8b34dc96599c703acad1705ac02ee4184165
Adam Svoboda <eidamsvoboda@gmail.com>
2016-12-06 09:29:23 +0100

    remove Windows directory

--- Windows/Screenshot.bat
+++
@@ -1,3 +0,0 @@
-adb shell screencap -p /mnt/sdcard/output.png
-adb pull /mnt/sdcard/output.png C:\Users\eidam\Desktop\screenshot"%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%".png
-adb shell rm /mnt/sdcard/output.png
